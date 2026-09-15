cf0fa9b369fcf8155af6ef172c0ea01f672200bd
wlndd <l1204926985@outlook.com>
2026-09-15 19:29:26 +0800

    build: bootstrap iRead Android app

---
+++ app/src/main/java/com/iread/novel/AppMetadata.kt
@@ -0,0 +1,6 @@
+package com.iread.novel
+
+object AppMetadata {
+    const val name = "iRead"
+    const val packageName = "com.iread.novel"
+}
---
+++ app/src/main/java/com/iread/novel/IReadApp.kt
@@ -0,0 +1,9 @@
+package com.iread.novel
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun IReadApp() {
+    Text("我的书架")
+}
